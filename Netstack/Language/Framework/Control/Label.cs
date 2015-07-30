ecc5da09b0d3ce70f1f2f437add240cbcac9f59c
Johan Geluk <jhgeluk@gmail.com>
2015-07-30 19:02:39 +0200

    Add label command

---
+++ Netstack/Language/Framework/Control/Label.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Netstack.Language.Literals;
+
+namespace Netstack.Language.Framework.Control
+{
+    class Label : Function
+    {
+        public override void Execute(NetStack stack)
+        {
+            var label = stack.Pop<string>();
+            stack.Push(new FunctionLabel(label));
+        }
+    }
+}

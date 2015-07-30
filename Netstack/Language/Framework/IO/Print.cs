dbbe63c22d9084e2e7f0314dc5e98fa570b23dc6
Johan Geluk <jhgeluk@gmail.com>
2015-07-30 19:03:18 +0200

    Add print command

---
+++ Netstack/Language/Framework/IO/Print.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Netstack.Language.Exceptions;
+
+namespace Netstack.Language.Framework.IO
+{
+    class Print:Function
+    {
+        public override void Execute(NetStack stack)
+        {
+            try
+            {
+                dynamic variable = stack.Pop();
+                Console.WriteLine(variable);
+            }
+            catch (StackEmptyException)
+            {
+                Console.WriteLine("[ ]");
+            }
+            
+        }
+    }
+}

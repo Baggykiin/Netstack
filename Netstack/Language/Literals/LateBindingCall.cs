caa9e8a014ac25c7a58854d3b2e35eeb573eb714
Johan Geluk <jhgeluk@gmail.com>
2015-07-30 19:27:21 +0200

    Improve LateBindingCall error handling

--- Netstack/Language/Literals/LateBindingCall.cs
+++ Netstack/Language/Literals/LateBindingCall.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CSharp.RuntimeBinder;
 
 namespace Netstack.Language.Literals
 {
-    class LateBindingCall :Literal
+    class LateBindingCall : Literal
     {
-        private string label;
+        private readonly string label;
         public LateBindingCall(string label)
         {
             this.label = label;
@@ -16,7 +17,17 @@ namespace Netstack.Language.Literals
 
         public override void Execute(NetStack stack)
         {
-            stack.GetFunction(label).Evaluate(stack);
+            Statement functionBody;
+            try
+            {
+                functionBody = stack.GetFunction(label);
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new RuntimeBinderException(string.Format("Failed to call function (no binding for \"{0}\" exists).", label));
+            }
+            
+            functionBody.Evaluate(stack);
         }
     }
 }

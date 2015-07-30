4937e63a2a4bdfc659f543abea442233a42db427
Johan Geluk <jhgeluk@gmail.com>
2015-07-30 19:03:04 +0200

    Add while command

---
+++ Netstack/Language/Framework/Control/While.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netstack.Language.Framework.Control
+{
+    class While : Function
+    {
+        public override void Execute(NetStack stack)
+        {
+            var condition = stack.Pop<Statement>();
+            var body = stack.Pop<Statement>();
+
+            condition.Evaluate(stack);
+            var result = stack.Pop<bool>();
+            while (result)
+            {
+                body.Evaluate(stack);
+                condition.Evaluate(stack);
+                result = stack.Pop<bool>();
+            }
+
+
+        }
+    }
+}
